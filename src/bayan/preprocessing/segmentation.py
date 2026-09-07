2bf2c43bcdb2e4994673327dab32b0dd52b2f1bd
itsdanah <itsdanahsaleh@gmail.com>
2026-09-07 10:07:47 +0300

    Complete sentence segmentation spot check

--- src/bayan/preprocessing/segmentation.py
+++ src/bayan/preprocessing/segmentation.py
@@ -1,11 +1,48 @@
 """Lab 1 starter: sentence segmentation."""
 
+import spacy
+from spacy.symbols import ORTH
+
+from .core import preprocess
 
 def build_pipeline():
     # TODO(Lab 1): build the spaCy segmentation pipeline.
-    raise NotImplementedError("Implement build_pipeline() in Lab 1")
+    nlp = spacy.blank("xx")
+
+    abbreviations = (
+        "Dr.",
+        "Mr.",
+        "Mrs.",
+        "Ms.",
+        "Prof.",
+        "e.g.",
+        "i.e.",
+        "د.",
+    )
+
+    for abbreviation in abbreviations:
+        nlp.tokenizer.add_special_case(
+            abbreviation,
+            [{ORTH: abbreviation}],
+        )
+
+    nlp.add_pipe(
+        "sentencizer",
+        config={"punct_chars": [".", "!", "?", "؟", "…"]},
+    )
+
+    return nlp
+    # raise NotImplementedError("Implement build_pipeline() in Lab 1")
 
 
 def split_sentences(raw: str, nlp) -> list[str]:
     # TODO(Lab 1): preprocess then return non-empty sentence strings.
-    raise NotImplementedError("Implement split_sentences() in Lab 1")
+    cleaned_text = preprocess(raw)
+    document = nlp(cleaned_text)
+
+    return [
+        sentence.text.strip()
+        for sentence in document.sents
+        if sentence.text.strip()
+    ]
+    # raise NotImplementedError("Implement split_sentences() in Lab 1")
